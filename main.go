0a7e6c2bc35aaf829d28dca27be49ed7b445571d
Zac Aguas <zacaguas@gmail.com>
2026-06-16 00:00:52 +0100

    feat: add debug log to file

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"log"
+	"os"
 
 	tea "charm.land/bubbletea/v2"
 )
 
 func main() {
+	if len(os.Getenv("DEBUG")) > 0 {
+		_ = os.Remove("debug.log")
+
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			log.Fatalln("fatal:", err)
+		}
+		defer f.Close()
+		getDataPath()
+	}
+
 	m := initialModel()
 	p := tea.NewProgram(m)
 	if _, err := p.Run(); err != nil {
